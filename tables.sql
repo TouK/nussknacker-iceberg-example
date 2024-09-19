b5759254ad0e470d656439cc7e203f2d926f09e5
Mateusz Kołodziejczyk <coutopl@gmail.com>
2024-09-19 14:49:31 +0200

    wip

--- tables.sql
+++ tables.sql
@@ -1,6 +1,3 @@
-docker compose -p nussknacker exec -it flink-jobmanager sql-client.sh
-
-a)
 CREATE CATALOG nessie_catalog WITH (
   'type'='iceberg',
   'catalog-impl'='org.apache.iceberg.nessie.NessieCatalog',
@@ -9,29 +6,28 @@ CREATE CATALOG nessie_catalog WITH (
   'warehouse' = 's3://warehouse',
   's3.endpoint'='http://minio:9000'
 );
-b):
+
 USE CATALOG nessie_catalog;
-c)
-CREATE database `default`;
-d)
-CREATE TABLE orders
+
+CREATE DATABASE IF NOT EXISTS `default`;
+
+CREATE TABLE IF NOT EXISTS orders
 (
    order_timestamp TIMESTAMP,
    order_date      STRING,
    id              BIGINT,
    customer_id     BIGINT
 ) PARTITIONED BY (order_date);
-e)
 
-CREATE TABLE products
+CREATE TABLE IF NOT EXISTS products
 (
    id            BIGINT,
    name          STRING,
    product_category TINYINT,
    current_prise DECIMAL(15, 2)
 );
-f)
-CREATE TABLE order_items
+
+CREATE TABLE IF NOT EXISTS order_items
 (
    id         BIGINT,
    order_id   BIGINT,
@@ -39,7 +35,15 @@ CREATE TABLE order_items
    quantity   BIGINT,
    unit_prise DECIMAL(15, 2)
 );
-g)
+
+CREATE TABLE IF NOT EXISTS product_orders_report
+(
+   report_id            BIGINT,
+   report_timestamp     TIMESTAMP,
+   report_year_month    STRING,
+   product_id           BIGINT,
+   product_quantity_sum BIGINT
+) PARTITIONED BY (report_year_month);
 
 INSERT INTO products (id, name, product_category, current_prise)
 VALUES (1, 'Basic Widget', 0, 9.99),
@@ -48,30 +52,20 @@ VALUES (1, 'Basic Widget', 0, 9.99),
       (4, 'Premium Gadget', 1, 29.99),
       (5, 'Basic Tool', 0, 24.99),
       (6, 'Premium Tool', 1, 49.99);
-h)
 
 INSERT INTO orders (order_timestamp, order_date, id, customer_id)
 VALUES (TO_TIMESTAMP('2024-09-12 14:25:30'), '2024-09-12', 1001, 2001),
       (TO_TIMESTAMP('2024-09-12 15:10:45'), '2024-09-12', 1002, 2002),
-      (TO_TIMESTAMP('2024-09-13 09:20:10'), '2024-09-13', 1003, 2003),
+      (TO_TIMESTAMP('2q024-09-13 09:20:10'), '2024-09-13', 1003, 2003),
       (TO_TIMESTAMP('2024-09-13 10:30:05'), '2024-09-13', 1004, 2004),
       (TO_TIMESTAMP('2024-09-13 12:15:22'), '2024-09-13', 1005, 2001),
       (TO_TIMESTAMP('2024-09-14 14:50:00'), '2024-09-14', 1006, 2002);
-i)
+
 INSERT INTO order_items (id, order_id, product_id, quantity, unit_prise)
-VALUES (10001, 1001, 1, 2, 9.99),  -- Order 1001 contains 2 Basic Widgets
+VALUES (10001, 1001, 1, 2, 9.99), -- Order 1001 contains 2 Basic Widgets
       (10002, 1001, 3, 1, 14.99), -- Order 1001 also contains 1 Basic Gadget
       (10003, 1002, 2, 1, 19.99), -- Order 1002 contains 1 Premium Widget
       (10004, 1003, 3, 3, 14.99), -- Order 1003 contains 3 Basic Gadgets
       (10005, 1004, 4, 1, 29.99), -- Order 1004 contains 1 Premium Gadget
       (10006, 1005, 5, 4, 24.99), -- Order 1005 contains 4 Basic Tools
       (10007, 1006, 6, 2, 49.99);
-j)
-CREATE TABLE product_orders_report
-(
-   report_id            BIGINT,
-   report_timestamp     TIMESTAMP,
-   report_year_month    STRING,
-   product_id           BIGINT,
-   product_quantity_sum BIGINT
-) PARTITIONED BY (report_year_month);
